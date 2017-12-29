c521260218c2264f449024de9de686730c852dfe
Duong Dang Hoang <alex@Duongs-iMac.local>
2017-12-29 20:17:56 +1100

    Initialization

---
+++ autoLayout/autoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  autoLayout
+//
+//  Created by Duong Dang Hoang on 29/12/17.
+//  Copyright © 2017 Duong Dang Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
